a3048c24d166b5b8a77f38acbc8335b238928df6
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-04-28 14:44:08 -0600

    service editing

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,15 +1,16 @@
-
 import monitor from "../assets/monitor.jpg";
 
 const Services = () => {
     return (
         <>
             <div>
-                <h2 className="text-center text-2xl font-black uppercase text-blue-900">Servicios</h2>
+                <h2 className="text-center text-2xl font-black uppercase text-blue-900">
+                    Servicios
+                </h2>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10">
-                <article className="bg-blue-800 rounded-lg shadow-xl p-10 text-white">
-                    <div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10 rounded">
+                <article className="bg-blue-800 rounded-lg shadow-xl text-white">
+                    <div className="w-full">
                         <img src={monitor} alt="monitor" className="w-full" />
                     </div>
 
@@ -22,7 +23,7 @@ const Services = () => {
                         <a href="#">Ver mas</a>
                     </button>
                 </article>
-                <article className="bg-blue-800 rounded-lg shadow-xl p-10 text-white">
+                <article className="bg-blue-800 rounded-lg shadow-xl text-white">
                     <div className="w-full">
                         <img src={monitor} alt="monitor" className="w-full" />
                     </div>
@@ -34,7 +35,7 @@ const Services = () => {
                         <a href="#">Ver mas</a>
                     </button>
                 </article>
-                <article className="bg-blue-800 rounded-lg shadow-xl p-10 text-white">
+                <article className="bg-blue-800 rounded-lg shadow-xl  text-white">
                     <div className="w-full">
                         <img src={monitor} alt="monitor" className="w-full" />
                     </div>
@@ -46,12 +47,10 @@ const Services = () => {
                         <a href="#">Ver mas</a>
                     </button>
                 </article>
-
-             
             </div>
             <button className="mb-10 font-black text-blue-900">
-                    <a href="#">Ver todos los servicios {">"}</a>
-                </button>
+                <a href="#">Ver todos los servicios {">"}</a>
+            </button>
         </>
     );
 };
