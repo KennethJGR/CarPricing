b89468275bda2e029381a553a3afea5ccf33f525
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-04-28 14:07:31 -0600

    footer editing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
 import logo from "../assets/logo.webp";
-import { AiFillFacebook,AiFillTwitterCircle } from "react-icons/ai";
+import {
+    AiFillFacebook,
+    AiFillTwitterCircle,
+    AiFillInstagram,
+    AiOutlineWhatsApp,
+    AiOutlineUser,
+    AiOutlineMail,
+} from "react-icons/ai";
+import { BsDiscord } from "react-icons/bs";
 
 const Footer = () => {
     return (
@@ -7,12 +15,12 @@ const Footer = () => {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10">
                 <div className="">
                     <img src={logo} alt="logo" className="w-40" />
-                    <h2
-                    className="text-xl"
-                    >Copyright 2023 - BarterCapital Todos los derechos reservados</h2>
+                    <h2 className="text-xl">
+                        Copyright 2023 - BarterCapital Todos los derechos reservados
+                    </h2>
                 </div>
 
-                <div className="justify-start items-center">
+                <div className="grid grid-rows-4">
                     <h2>
                         <p className="font-black text-2xl">Redes sociales</p>
                     </h2>
@@ -27,8 +35,7 @@ const Footer = () => {
                         </p>
                     </h2>
                     <h2 className="flex justify-center gap-3  mb-2  ">
-                        < AiFillTwitterCircle
-                             className="text-3xl" />
+                        <AiFillTwitterCircle className="text-3xl" />
 
                         <p className="text-center text-xl font-black">
                             <a href="https://www.facebook.com/BarterCapital-100101632425227">
@@ -37,20 +44,20 @@ const Footer = () => {
                         </p>
                     </h2>
                     <h2 className="flex justify-center gap-3  mb-2  ">
-                        <AiFillFacebook className="text-3xl" />
+                        <AiFillInstagram className="text-3xl" />
 
                         <p className="text-center text-xl font-black">
                             <a href="https://www.facebook.com/BarterCapital-100101632425227">
-                                Facebook
+                                Instagram
                             </a>
                         </p>
                     </h2>
                     <h2 className="flex justify-center gap-3  mb-2  ">
-                        <AiFillFacebook className="text-3xl" />
+                        <BsDiscord className="text-3xl" />
 
                         <p className="text-center text-xl font-black">
                             <a href="https://www.facebook.com/BarterCapital-100101632425227">
-                                Facebook
+                                Discord
                             </a>
                         </p>
                     </h2>
@@ -58,21 +65,31 @@ const Footer = () => {
 
                 <div>
                     <h2>
-                        <p className="font-black">Contacto</p>
+                        <p className="font-black text-2xl">Contacto</p>
                     </h2>
-                    <h2>
-                        <p className="text-center">
-                            <a href="https://www.instagram.com/bartercapital/">WhatsApp</a>
+                    <h2 className="flex justify-center gap-3  mb-2  ">
+                        <AiOutlineWhatsApp className="text-3xl" />
+
+                        <p className="text-center text-xl font-black">
+                            <a href="https://www.facebook.com/BarterCapital-100101632425227">
+                                WhatsApp
+                            </a>
                         </p>
                     </h2>
-                    <h2>
-                        <p className="text-center">
-                            <a href="https://www.instagram.com/bartercapital/">Soporte</a>
+                    <h2 className="flex justify-center gap-3  mb-2  ">
+                        <AiOutlineUser className="text-3xl" />
+
+                        <p className="text-center text-xl font-black">
+                            <a href="https://www.facebook.com/BarterCapital-100101632425227">
+                                Soporte
+                            </a>
                         </p>
                     </h2>
-                    <h2>
-                        <p className="text-center">
-                            <a href="https://www.instagram.com/bartercapital/">
+                    <h2 className="flex justify-center gap-3  mb-2  ">
+                        <AiOutlineMail className="text-3xl" />
+
+                        <p className="text-center text-xl font-black">
+                            <a href="https://www.facebook.com/BarterCapital-100101632425227">
                                 Correo Electronico
                             </a>
                         </p>
